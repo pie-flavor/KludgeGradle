776434900b0d0952ab6b2568ec1e850558083a81
Adam Spofford <pieflavor.mc@gmail.com>
2019-06-18 20:05:35 -0700

    Bump compiler plugin version

--- src/main/kotlin/KludgeGradleSubplugin.kt
+++ src/main/kotlin/KludgeGradleSubplugin.kt
@@ -28,7 +28,7 @@ class KludgeGradleSubplugin : KotlinGradleSubplugin<AbstractCompile> {
 
     override fun getCompilerPluginId(): String = "kludge"
 
-    override fun getPluginArtifact(): SubpluginArtifact = SubpluginArtifact("com.github.pie-flavor", "KludgeCompiler", "f5ce6a2")
+    override fun getPluginArtifact(): SubpluginArtifact = SubpluginArtifact("com.github.pie-flavor", "KludgeCompiler", "9b7af37")
 
     override fun isApplicable(project: Project, task: AbstractCompile): Boolean =
             project.plugins.hasPlugin(KludgeGradlePlugin::class.java)
